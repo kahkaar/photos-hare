b87fd0c2b47c4f0356308d0f4f5803cce334fab4
Aaron <119842456+kahkaar@users.noreply.github.com>
2025-09-06 12:04:33 +0300

    Update schema files
    Fixed 'posts' trigger to behave correctly
    Added triggers for 'users' and 'post_comments' tables
    Changed 'image BLOB' to 'image_path TEXT'
    Updated dummy data for 'image_path'

--- db/dummy.sql
+++ db/dummy.sql
@@ -1,24 +1,25 @@
--- Generated using SQL Test Data Generator Tool from https://www.coderstool.com/sql-test-data-generator
-INSERT INTO posts (title, description)
-VALUES ('canon',  'prepared feels beverly situated statute');
-INSERT INTO posts (title, description)
-VALUES ('sailing', 'collected operations grand yale parameter');
-INSERT INTO posts (title, description)
-VALUES ('tba', 'gulf featuring handheld supported pix');
-INSERT INTO posts (title, description)
-VALUES ('sorted', 'morris abilities outcomes optional keys');
-INSERT INTO posts (title, description)
-VALUES ('corporation', 'casino european leads gay function');
-INSERT INTO posts (title, description)
-VALUES ('xi', 'development work apollo miami nike');
-INSERT INTO posts (title, description)
-VALUES ('awareness', 'tree weights searching trinidad slides');
-INSERT INTO posts (title, description)
-VALUES ('downloads', 'years missions lowest ambassador arrives');
-INSERT INTO posts (title, description)
-VALUES ('expense', 'adams coaches achievements kenny need');
-INSERT INTO posts (title, description)
-VALUES ('highlight', 'opens cbs display future mailman');
+BEGIN;
+-- Partly generated using SQL Test Data Generator Tool from https://www.coderstool.com/sql-test-data-generator
+INSERT INTO posts (title, description, image_path)
+VALUES ('canon',  'prepared feels beverly situated statute', 'path/to/image/');
+INSERT INTO posts (title, description, image_path)
+VALUES ('sailing', 'collected operations grand yale parameter', 'path/to/image/');
+INSERT INTO posts (title, description, image_path)
+VALUES ('tba', 'gulf featuring handheld supported pix', 'path/to/image/');
+INSERT INTO posts (title, description, image_path)
+VALUES ('sorted', 'morris abilities outcomes optional keys', 'path/to/image/');
+INSERT INTO posts (title, description, image_path)
+VALUES ('corporation', 'casino european leads gay function', 'path/to/image/');
+INSERT INTO posts (title, description, image_path)
+VALUES ('xi', 'development work apollo miami nike', 'path/to/image/');
+INSERT INTO posts (title, description, image_path)
+VALUES ('awareness', 'tree weights searching trinidad slides', 'path/to/image/');
+INSERT INTO posts (title, description, image_path)
+VALUES ('downloads', 'years missions lowest ambassador arrives', 'path/to/image/');
+INSERT INTO posts (title, description, image_path)
+VALUES ('expense', 'adams coaches achievements kenny need', 'path/to/image/');
+INSERT INTO posts (title, description, image_path)
+VALUES ('highlight', 'opens cbs display future mailman', 'path/to/image/');
 
 -- Same as init.sql; not generated.
 INSERT INTO tags (name) VALUES ('cats');
@@ -27,3 +28,4 @@ INSERT INTO tags (name) VALUES ('dogs');
 INSERT INTO tags (name) VALUES ('finland');
 INSERT INTO tags (name) VALUES ('pets');
 INSERT INTO tags (name) VALUES ('rabbits');
+END;
--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,11 @@
+BEGIN;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS post_comments;
 DROP TABLE IF EXISTS tags;
 DROP TABLE IF EXISTS posts_tags;
 
--- Triggers associated with 'table' are dropped on DROP TABLE [IF EXISTS] 'table'.
+-- N.B. Triggers associated with 'table' are dropped on `DROP TABLE [IF EXISTS] 'table'`.
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
@@ -18,7 +19,7 @@ CREATE TABLE posts (
   id INTEGER PRIMARY KEY,
   title TEXT NOT NULL,
   description TEXT,
-  image BLOB, -- Add NOT NULL
+  image_path TEXT NOT NULL, -- Store images inside a directory, instead of the database itself.
   unlisted BOOLEAN NOT NULL DEFAULT FALSE,
   created_at INTEGER NOT NULL DEFAULT (UNIXEPOCH('now')),
   updated_at INTEGER,
@@ -30,8 +31,9 @@ CREATE TABLE post_comments (
   text TEXT NOT NULL,
   likes INTEGER NOT NULL DEFAULT 0,
   created_at INTEGER NOT NULL DEFAULT (UNIXEPOCH('now')),
-  user_id INTEGER REFERENCES users,
-  post_id INTEGER REFERENCES posts
+  updated_at INTEGER,
+  post_id INTEGER REFERENCES posts,
+  user_id INTEGER REFERENCES users
 );
 
 CREATE TABLE tags (
@@ -45,9 +47,24 @@ CREATE TABLE posts_tags (
   tag_id INTEGER REFERENCES tags
 );
 
-CREATE TRIGGER posts_update_timestamp_trigger AFTER UPDATE ON posts
+CREATE TRIGGER users_updated_at_trigger AFTER UPDATE ON users
+  BEGIN
+    UPDATE users
+    SET updated_at = UNIXEPOCH('now')
+    WHERE id = old.id;
+  END;
+
+CREATE TRIGGER posts_updated_at_trigger AFTER UPDATE ON posts
   BEGIN
     UPDATE posts
     SET updated_at = UNIXEPOCH('now')
-    WHERE old.id = new.id;
+    WHERE id = old.id;
   END;
+
+CREATE TRIGGER post_comments_updated_at_trigger AFTER UPDATE ON post_comments
+  BEGIN
+    UPDATE post_comments
+    SET updated_at = UNIXEPOCH('now')
+    WHERE id = old.id;
+  END;
+END;
