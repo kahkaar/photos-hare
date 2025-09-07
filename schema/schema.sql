ba7d30f29e69a009c9e1ab915d4f8c0fa78b4641
Aaron <119842456+kahkaar@users.noreply.github.com>
2025-09-07 11:50:51 +0300

    Change db/ to schema/
    Changed ids to be 16 byte BLOBs
    Changed posts title to be null
    Added ON DELETE CASCADE to relevant columns

--- schema/schema.sql
+++ schema/schema.sql
@@ -8,43 +8,44 @@ DROP TABLE IF EXISTS posts_tags;
 -- N.B. Triggers associated with 'table' are dropped on `DROP TABLE [IF EXISTS] 'table'`.
 
 CREATE TABLE users (
-  id INTEGER PRIMARY KEY,
+  id BLOB PRIMARY KEY DEFAULT (LOWER(HEX(RANDOMBLOB(16)))),
   username TEXT NOT NULL UNIQUE,
   password_hash TEXT NOT NULL,
   created_at INTEGER NOT NULL DEFAULT (UNIXEPOCH('now')),
-  updated_at INTEGER
+  updated_at INTEGER,
+  CHECK(LENGTH(username) >= 4)
 );
 
 CREATE TABLE posts (
-  id INTEGER PRIMARY KEY,
-  title TEXT NOT NULL,
+  id BLOB PRIMARY KEY DEFAULT (LOWER(HEX(RANDOMBLOB(16)))),
+  title TEXT,
   description TEXT,
   image_path TEXT NOT NULL, -- Store images inside a directory, instead of the database itself.
   unlisted BOOLEAN NOT NULL DEFAULT FALSE,
   created_at INTEGER NOT NULL DEFAULT (UNIXEPOCH('now')),
   updated_at INTEGER,
-  user_id INTEGER REFERENCES users
+  user_id BLOB REFERENCES users ON DELETE CASCADE
 );
 
 CREATE TABLE post_comments (
-  id INTEGER PRIMARY KEY,
+  id BLOB PRIMARY KEY DEFAULT (LOWER(HEX(RANDOMBLOB(16)))),
   text TEXT NOT NULL,
   likes INTEGER NOT NULL DEFAULT 0,
   created_at INTEGER NOT NULL DEFAULT (UNIXEPOCH('now')),
   updated_at INTEGER,
-  post_id INTEGER REFERENCES posts,
-  user_id INTEGER REFERENCES users
+  post_id BLOB REFERENCES posts ON DELETE CASCADE,
+  user_id BLOB REFERENCES users ON DELETE CASCADE
 );
 
 CREATE TABLE tags (
-  id INTEGER PRIMARY KEY,
+  id BLOB PRIMARY KEY DEFAULT (LOWER(HEX(RANDOMBLOB(16)))),
   name TEXT NOT NULL UNIQUE,
   created_at INTEGER NOT NULL DEFAULT (UNIXEPOCH('now'))
 );
 
 CREATE TABLE posts_tags (
-  post_id INTEGER REFERENCES posts,
-  tag_id INTEGER REFERENCES tags
+  post_id BLOB REFERENCES posts ON DELETE CASCADE,
+  tag_id BLOB REFERENCES tags ON DELETE CASCADE
 );
 
 CREATE TRIGGER users_updated_at_trigger AFTER UPDATE ON users
@@ -67,4 +68,9 @@ CREATE TRIGGER post_comments_updated_at_trigger AFTER UPDATE ON post_comments
     SET updated_at = UNIXEPOCH('now')
     WHERE id = old.id;
   END;
+
+CREATE UNIQUE INDEX idx_username ON users (username);
+CREATE UNIQUE INDEX idx_posts_title ON posts (title);
+CREATE UNIQUE INDEX idx_posts_unlisted_user_id ON posts (unlisted, user_id);
+
 END;
